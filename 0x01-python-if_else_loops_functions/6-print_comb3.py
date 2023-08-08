79cc389287d068719554a57977ed34073668cd74
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-08 10:32:44 -0700

    printcomb

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for dig in range(0, 10):
+    for dig1 in range(dig + 1, 10):
+        if dig == 8 and dig1 ==9:
+            print("{}{}".format(dig, dig1))
+        else:
+            print("{}{}".format(dig, dig1), end=", ")
