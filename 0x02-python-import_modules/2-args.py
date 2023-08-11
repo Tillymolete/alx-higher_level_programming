b799f6e1675f0419c918bc6a6809bda9a37b7f11
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-11 09:15:37 +0000

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+	argv = sys.argv[1:]
+	num_args = len(argv)
+	
+	if num_args == 1:
+		print("Number of arguments: 1:", end=" ")
+	else:
+		print("Number of arguments: {}:".format(num_args), end=" ")
+	
+	for i, arg in enumerate(argv, start=1):
+		print("{}: {}".format(i, arg))
+	if num_args == 0:
+		print(".", end=" ")
