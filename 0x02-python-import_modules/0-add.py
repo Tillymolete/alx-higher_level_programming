2a75d4286fc0d6b6cc98a5114ea341a7b30c4e33
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-11 07:00:09 +0000

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add(a, b):
-a = 1
-b = 2
+
+	a = 1
+	b = 2
 
 from add_0 import add
 result = add(a, b)
-print(f"{a} + {b} = {result}")
+print(f"{} + {} = {}".format(a, b, add(a, b)))
