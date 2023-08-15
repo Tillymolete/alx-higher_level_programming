cebaf2bcfa7c7fb51eae636f30b7dabe7c8f2ab3
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-15 08:41:26 +0000

    printpythonlist

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+Py_ssize_t size, alloc;
+Py_ssize_t i;
+PyObject *item;
+
+if (!PyList_Check(p)) {
+fprintf(stderr, "Invalid object passed!\n");
+return;
+}
+
+size = PyList_Size(p);
+alloc = ((PyListObject *)p)->allocated;
+
+printf("[*] Size of the Python List = %zd\n", size);
+printf("[*] Allocated = %zd\n", alloc);
+
+for (i = 0; i < size; i++) {
+item = PyList_GetItem(p, i);
+printf("element %zd: %s\n", i Py_TYPE(iytem)->tp_name);
+}
+}
