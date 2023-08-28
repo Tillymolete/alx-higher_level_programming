bbc5514dc51e4238e41d95a4a9bc5f247c475aa4
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-28 17:55:18 +0000

    printdivision

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+	try:
+		count = 0
+		for element in my_list:
+			if count>= x:
+				break
+			if isinstance(element, int):
+				print("{:d}".format(element), end='')
+				count += 1
+	except:
+		pass
+	finally:
+		print('')
+	return count
