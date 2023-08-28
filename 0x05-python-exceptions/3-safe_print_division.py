60234ef7222d4f6864b0381cbe93952711a6578c
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-28 18:03:35 +0000

    printdivision

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
 	try:
-		count = 0
-		for element in my_list:
-			if count>= x:
-				break
-			if isinstance(element, int):
-				print("{:d}".format(element), end='')
-				count += 1
+		result = a / b	
 	except:
-		pass
+		result = None
 	finally:
-		print('')
-	return count
+		print("Inside result: ""{}".format(result))
+	return result
