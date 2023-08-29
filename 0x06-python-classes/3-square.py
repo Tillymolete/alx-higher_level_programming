f8893146d2141339721b91eb8398e7159fa82c40
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-29 17:52:13 +0000

    changederrors

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+"""Defination of class 'Square' with privet instance attribute"""
+
+
 class Square:
-	def __init__(self, size=0):
-		if not isinstance(size, int):
-			raise TypeError("size must be an integer")
-		if size < 0:
-			raise ValueError("size must be >= 0")
-		self.__size =  size
-	def area(self):
-		return self. __size ** 2
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Inits Square with privet instance attribute 'size'
+
+        Args:
+            size: Size of the square with integer value
+
+        Raises:
+            TypeError: When size is not integer
+            ValueError: When size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size * self.__size
