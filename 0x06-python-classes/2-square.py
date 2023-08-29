c50d8688a5b2ed5cbf2c71a2a1675edc7d7410a8
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-29 16:47:56 +0000

    sizevalidation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defination of class 'Square' with privet instance attribute"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Inits Square with privet instance attribute 'size'
+
+        Args:
+            size: Size of the square with integer value
+
+        Raises:
+            TypeError: When size is not integer
+            ValueError: When size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
