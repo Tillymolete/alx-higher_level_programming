a323c4e064f336762cbf637bf3335c1c116e2f2d
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-29 05:40:17 +0000

    changes  to code

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
 			result = 0
 			print("out of range")
 		finally:
-			new_list[i] = result
-		return (new_list)
+			new_list[i]  = result
+	return new list
