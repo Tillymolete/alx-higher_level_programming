625983258e65c3b5401a3bd048cb491f4c16720d
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-06 05:34:20 +0000

    simplerectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" class modulde for an empty class rectangle"""
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """ Empty class called rectangle """
+    """Represent a rectangle."""
     pass
