02b8216fcd770eb9b679eb80b9034de7e304f03c
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-12 16:00:37 +0000

    readdatafile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" A module containing definition for reed_file function"""
+""" A module containing defination for the read_file function"""
+
 
 def read_file(filename=""):
-	"""Function to read data from a file
-	Args:
-		filename (str: name of the file to read
-	"""
-	with open(filename, "r", encoding="utf-8") as f:
-		read_data =  f.read()
-		print(read_data, end=""
+    """ A function that reads data from a file
+
+    Args:
+        filename: the name of the file to be read
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
