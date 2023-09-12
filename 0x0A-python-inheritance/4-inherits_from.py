9963a227b76460e898d867f522477a25bc16e3c4
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-12 07:36:27 +0000

    inhertsfrom

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!usr/bin/python3
-"""A module for function called is_kind_class"""
+"""A module for function called inherit_from"""
 
 def inherits_from(obj, a_class):
 	"""Function that defines whether object is an instance of a class that inherited
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
 	Return:
 		True if the object is an instance of a class otherwise false
 	"""
-	return isinstance(obj, a_class)
+	return isinstance(obj, a_class) and type(obj) != a_class
