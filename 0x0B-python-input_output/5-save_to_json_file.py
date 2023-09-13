89b63d68d1ba0ade5bd9bb9700c3dcf385be1971
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-13 08:43:56 +0000

    savetojson

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""A function that writes object save"""
+""" A module that defines a function called save_to_json_file.py"""
+
 
 import json
 
-def save_to_json_file(my_obj, filename):
-	"""function that writes an Object to a text file, using a JSON representation
 
-	Args:
-		my_obj: the object representation
-		filename: the file 
-	Return: the object for json
-	"""
-	
-	with open(filename, "w", encoding="uft-8") as file:
-		json.dump(my_obj, f)
+def save_to_json_file(my_obj, filename):
+    """ A function that  writes an Object to a
+        text file, using a JSON representation
+    Args:
+        my_obj: the object
+        filename: the path to the file to be written on
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
