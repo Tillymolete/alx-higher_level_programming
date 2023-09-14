d371490558f00ee566fa21d53e1d673e0358f4d2
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-14 06:51:05 +0000

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""A class module called student"""
+"""Defines a class Student."""
+
 
 class Student:
-	""" A constractor method for the Student class
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
 
         Args:
-            first_name: the first name of a person instance
-            last_name: the last_name of a person instance
-            age: the age of a person instance
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
-    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-	""" A method that retrieves a dictionary representation
-            of a Student instance
-        Returns:
-            the dictionary representation of a Student instance
-        """
-        result = {}
-        if attrs is None:
-            return self.__dict__
-        for attr in attrs:
-            if hasattr(self, attr):
-                result[attr] = getattr(self, attr)
-        return result
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
