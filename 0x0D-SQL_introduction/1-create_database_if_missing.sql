5723ac04d4d3ff20ea469c8674e7aed9a25d7a16
Tillymolete <Mahlatsemolete@gmail.com>
2023-10-17 09:38:54 -0700

    createdatabase

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
---Script that creates the database hbtn_0c_0 in MySQL server
+-- A sql script that creates a new database if it doesn't already exists
+-- The CREATE querie creates a new database
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
