82c288536ad724b0f5f2d45ef139b8f4c19b66ea
Tillymolete <Mahlatsemolete@gmail.com>
2023-10-17 09:12:02 -0700

    fixed error

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
---lists all databases of your MySQL server
---This will list all dtabase in a server using 'SHOW DATABASES'
+--Lists all databases of your MySQL server.
+--This will list all dtabase in a server using 'SHOW DATABASES'.
 SHOW DATABASES;
