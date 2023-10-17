fd8681e79919788968ba01bf4424686ec1c761fd
Tillymolete <Mahlatsemolete@gmail.com>
2023-10-17 10:18:23 -0700

    fullcreation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- A cript that creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table
+{
+id INT, 
+name VARCHAR(256)
+score INT
+};
+
+-- insert the data
+
+INSERT INTO second_table(id, name, score)
+VALUE
+(1, 'John', 10)
+,(2, 'Alex', 3)
+,(3, 'Bob', 14)
+,(4, 'George', 8);
