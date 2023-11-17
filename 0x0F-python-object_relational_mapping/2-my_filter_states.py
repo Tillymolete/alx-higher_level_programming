edb106db788ee32246de41029b7888e3e87cee20
Tillymolete <Mahlatsemolete@gmail.com>
2023-11-17 10:57:01 -0800

    filteredsates

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+
+    args = sys.argv
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3])
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+    cur.execute(query.format(sys.argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
