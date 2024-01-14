abb28a878ec05db136d32e2466b92ef31a20d4b9
Tillymolete <mahlatsemolete@gmail.com>
2024-01-14 03:44:07 -0800

    statefetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa
+A script that lists all State objects from the database hbtn_0e_6_usa
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -9,6 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
