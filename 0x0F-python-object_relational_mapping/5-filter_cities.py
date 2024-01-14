17aaa484803edab5787f6fc46b76c739ac2bec06
Tillymolete <mahlatsemolete@gmail.com>
2024-01-14 03:30:50 -0800

    filtercitie

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """
-List all cities of a state
+a script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
 """
+
+
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = '{}';".format(sys.argv[4]))
-    states = cur.fetchall()
+    args = sys.argv
 
-    print(", ".join([state[1] for state in states]))
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3])
+    cur = conn.cursor()
+    query = ("SELECT cities.name FROM cities INNER JOIN states "
+             "ON cities.state_id = states.id AND states.name = %s "
+             "ORDER BY cities.id")
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+
+    print(", ".join([row[0] for row in rows]))
+
+    cur.close()
+    conn.close()
