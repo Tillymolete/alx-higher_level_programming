6d18dfca613f4d83e4a49cd8aed3cbee0e33c8b5
Tillymolete <mahlatsemolete@gmail.com>
2024-01-14 04:04:54 -0800

    statefilter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter a from the database
+A script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -9,6 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
@@ -19,3 +22,5 @@ if __name__ == '__main__':
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    session.close()
