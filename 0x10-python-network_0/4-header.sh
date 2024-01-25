1f5a47895dbccf901135cb1aacacfe1ee1626572
Tillymolete <mahlatsemolete@gmail.com>
2024-01-25 07:59:51 -0800

    fixed error

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# A script to sent GET request
+# A script to sent GET request to URL
 curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# A script that sends a POST request to url
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
