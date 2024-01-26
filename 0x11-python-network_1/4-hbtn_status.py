4077019a3ab267973166d0b0c294c24d6a6d37b1
Tillymolete <Mahlatsemolete@gmail.com>
2024-01-26 04:29:28 -0800

    fetchstatus

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    text = req.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(text)))
+    print('\t- content: {}'.format(text))
